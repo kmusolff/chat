14ef6e08f097c78d9396e309b0e12cf653e1f9c6
Karol Musolff <karolmusolff@gmail.com>
2015-10-11 14:43:04 +0200

    WIP

--- app.js
+++ app.js
@@ -3,25 +3,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname));
 
 app.engine('.html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
 
 app.get('/', function(req, res){
-  res.render('index.html');
+  res.render('index');
 });
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-  	socket.on('disconnect', function(){
-  	console.log('user disconnected');
-  });
-  socket.on('chat message', function(data){
-  	console.log(data.date +  ' ' + data.author + ': ' + data.text)
-  	io.emit('chat message', data);
-  });
-});
-
-http.listen(80, function(){
-  console.log('listening on *:80');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
